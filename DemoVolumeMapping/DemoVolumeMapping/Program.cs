35950d2df2edae05e7f5c4feffffcbfe6dea1532
Johan Smarius <jsmarius@bergler.nl>
2018-03-16 22:35:23 +0100

    Initial version

---
+++ DemoVolumeMapping/DemoVolumeMapping/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace DemoVolumeMapping
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            int counter = Convert.ToInt32(args[0]);
+
+            string fileName = args[1];
+
+            using (var stream = new StreamWriter(fileName))
+            {
+                for (int i = 0; i < counter; i++)
+                {
+                    stream.WriteLine($"Line: {i}");
+                }
+
+                stream.Flush();
+            }
+
+            return 0;
+        }
+    }
+}
